02e3abb2eb914eaaea7abd05e7384809cc5b2580
judy1230 <50729317+judy1230@users.noreply.github.com>
2020-03-25 15:10:12 +0800

    add navbar

---
+++ src/views/SignUp.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="container py-5">
+    <form class="w-100" @submit.prevent.stop="handleSubmit">
+      <div class="text-center mb-4">
+        <h1 class="h3 mb-3 font-weight-normal">
+          Sign Up
+        </h1>
+      </div>
+
+      <div class="form-label-group mb-2">
+        <label for="name">Name</label>
+        <input
+          id="name"
+					v-model="name"
+          name="name"
+          type="text"
+          class="form-control"
+          placeholder="name"
+          required
+          autofocus
+        >
+      </div>
+
+      <div class="form-label-group mb-2">
+        <label for="email">Email</label>
+        <input
+          id="email"
+					v-model="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="email"
+          required
+        >
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password">Password</label>
+        <input
+          id="password"
+					v-model="password1"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          required
+        >
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password-check">Password Check</label>
+        <input
+          id="password-check"
+					v-model="password2"
+          name="passwordCheck"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          required
+        >
+      </div>
+
+      <button
+        class="btn btn-lg btn-primary btn-block mb-3"
+        type="submit"
+      >
+        Submit
+      </button>
+
+      <div class="text-center mb-3">
+        <p>
+          <router-link to="/signin">
+            Sign In
+          </router-link>
+        </p>
+      </div>
+
+      <p class="mt-5 mb-3 text-muted text-center">
+        &copy; 2017-2018
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  data () {
+    return {
+			name:'',
+      email: 'user1@example.com',
+			password1: '12345678',
+			password2: '12345678'
+
+    }
+	},
+	methods: {
+		// eslint-disable-next-line
+		handleSubmit (e) {
+			const data = JSON.stringify({
+				name: this.name,
+				email: this.email,
+				password1: this.password1,
+				password2: this.password2
+			})
+			// eslint-disable-next-line
+			consoel.log('data', data)
+		}
+	}
+}
+</script>
