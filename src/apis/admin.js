016aa079841cf58891afdfae4912987cd03976a2
judy1230 <50729317+judy1230@users.noreply.github.com>
2020-04-18 16:54:54 +0800

    add adminAPI.restaurants.update to AdminRestaurantEdit.vue

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -4,15 +4,30 @@ const getToken = () => localStorage.getItem('token')
 export default {
 	categories: {
 		get() {
-			console.log('getToken()', getToken())
 			return apiHelper.get('/admin/categories', {
 				headers: { Authorization: `Bearer ${getToken()}`}})
 		}
 	},
 	restaurants: {
+		getDetail({ restaurantId }) {
+			return apiHelper.get(`/admin/restaurants/${restaurantId}`, {
+				headers: { Authorization: `Bearer ${getToken()}` }
+			})
+		},
+		get() {
+			return apiHelper.get('/admin/restaurants', { headers: { Authorization: `Bearer ${getToken()}` } })
+		},
 		create({ formData }) {
-			console.log('getToken()', getToken())
 			return apiHelper.post('/admin/restaurants', formData, { headers: { Authorization: `Bearer ${getToken()}` } })
+		},
+		update({ restaurantId, formData }) {
+			return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, {
+				headers: { Authorization: `Bearer ${getToken()}` }
+			})
+		},
+		delete({ restaurantId }) {
+			console.log('getToken()', getToken())
+			return apiHelper.delete(`/admin/restaurants/${restaurantId}`, { headers: { Authorization: `Bearer ${getToken()}` }})
 		}
 	}
 
